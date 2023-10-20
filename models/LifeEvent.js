5f1340d8774a86b1fb73415358d781805990110b
AlfG71 <56656187+AlfG71@users.noreply.github.com>
2023-10-20 15:32:04 -0700

    added events routes

--- models/LifeEvent.js
+++ models/LifeEvent.js
@@ -2,24 +2,40 @@ const { Schema, model } = require("mongoose");
 
 const lifeEventSchema = new Schema(
   {
-    child: {
-      type: { type: Schema.Types.ObjectId, ref: "Child" },
-      required: true,
-    },
+    child: { type: Schema.Types.ObjectId, ref: "Child" } ,
+
     eventTitle: String,
+
     date: {
+
       type: Date,
       required: true,
+
     },
+
     description: {
+
       type: String,
       required: true,
+
     },
-    img: String,
+
+    img: {
+
+      type: String,
+      default:
+        "https://cvhrma.org/wp-content/uploads/2015/07/default-profile-photo.jpg",
+
+    },
+
   },
+
   {
+
     timestamps: true,
+
   }
+
 );
 
 module.exports = model("LifeEvent", lifeEventSchema);
