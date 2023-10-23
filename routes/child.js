4ed8737ab0c32f6dc214127b39d53dba0882f944
AlfG71 <56656187+AlfG71@users.noreply.github.com>
2023-10-23 13:22:15 -0700

    modified child route

--- routes/child.js
+++ routes/child.js
@@ -18,9 +18,9 @@ router.post("/create", isAuthenticated, (req, res, next) => {
     .then((toPopulate) => {
       return toPopulate.populate("children");
     })
-    .then((userNewChild) => {
-      const { _id, name, img, dateOfBirth } = userNewChild;
-      const user = { _id, name, img, dateOfBirth };
+    .then((updatedUser) => {
+      const { _id, userName, img, email, children } = updatedUser;
+      const user = { _id, userName, img, email, children };
       const authToken = jwt.sign(user, process.env.SECRET, {
         algorithm: "HS256",
         expiresIn: "6h",
@@ -38,7 +38,7 @@ router.post("/create", isAuthenticated, (req, res, next) => {
 //Check
 router.get('/profile/:childId', (req, res, next) => {
   const childId  = req.params.childId
-  
+
   Child.findById(childId)
     .then((foundChild) => {
       const { name, dateOfBirth, img, events } = foundChild
@@ -63,8 +63,8 @@ router.post('/edit-child/:childId', (req, res, next) => {
 
   Child.findByIdAndUpdate( childId, req.body, {new: true })
     .then((foundChild) => {
-     const { name, dateOfBirth, img, events } = foundChild 
-      
+     const { name, dateOfBirth, img, events } = foundChild
+
      const child = { name, dateOfBirth, img, events }
      res.status(201).json('child updated')
     })
